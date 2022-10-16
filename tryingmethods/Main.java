50094e4e08e31b5d36181463423a22a9e17616fb
clintonjosephs <clintonjosephs26@gmail.com>
2022-10-16 14:29:02 +0000

    Made changes to day arrangement

--- tryingmethods/Main.java
+++ tryingmethods/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         TryingMethods tryingMethods = new TryingMethods();
-
         // Testing the dayOfWeek method
         for (int j = 1; j < 8; j++) {
             System.out.println(tryingMethods.dayOfWeek(j));
@@ -9,8 +8,11 @@ public class Main {
 
         // Testing the printStatement method
          tryingMethods.printStatement(90);
+         TryingMethods.changeName("Clinton");
          tryingMethods.printStatement(40);
+         TryingMethods.changeName("John Doe");
          tryingMethods.printStatement(50);
+
 
         // Testing the sumLessthanOneThousand method
         System.out.println(TryingMethods.sumLessthanOneThousand());
--- tryingmethods/TryingMethods.java
+++ tryingmethods/TryingMethods.java
@@ -1,8 +1,14 @@
 class TryingMethods {
+    static String userName = "";
     /**
         This class is used to test out methods, conditional statements, 
         iterations/loops, method types (Static / Dynamic) 
     */ 
+  
+    public static void changeName(String name) {
+        userName = name;
+    }
+
     public String dayOfWeek(int day) {
         /**
             This method receives an integer and returns a string, the integer is received
@@ -11,25 +17,25 @@ class TryingMethods {
         String dayOfWeek = "";
         switch (day) {
             case 1:
-                dayOfWeek = "Monday";
+                dayOfWeek = "Sunday";
                 break;
             case 2:
-                dayOfWeek = "Tuesday";
+                dayOfWeek = "Monday";
                 break;
             case 3:
-                dayOfWeek = "Wednesday";
+                dayOfWeek = "Tuesday";
                 break;
             case 4:
-                dayOfWeek = "Thursday";
+                dayOfWeek = "Wednesday";
                 break;
             case 5:
-                dayOfWeek = "Friday";
+                dayOfWeek = "Thursday";
                 break;
             case 6:
-                dayOfWeek = "Saturday";
+                dayOfWeek = "Friday";
                 break;
             case 7:
-                dayOfWeek = "Sunday";
+                dayOfWeek = "Saturday";
                 break;
             default:
                 dayOfWeek = "Invalid day";
@@ -39,7 +45,6 @@ class TryingMethods {
     }
 
     public void printStatement(int score) {
-        String name = "Clinton";
         /**
             This method receives an integer and 
             prints a statement based on the 
@@ -53,7 +58,7 @@ class TryingMethods {
             System.out.println("You got a C");
         } else if (score >= 60) {
             System.out.println("You got a D");
-        } else if (score < 60 && name == "Clinton") {
+        } else if (score < 60 && userName == "Clinton") {
             System.out.println("You passed 😀");
         } else {
             System.out.println("You got an F");
