9623c9fa60add1aed578463ac61f7142c7beca8e
clintonjosephs <clintonjosephs26@gmail.com>
2022-11-02 16:24:10 +0000

    Finalise today's class

--- classwork/Main.java
+++ classwork/Main.java
@@ -1,12 +1,15 @@
 public class Main {
+    // Enumerators are used to define a set of constants
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int examScore = 90;
-        float examScore2 = 90.0f;
-        boolean isPassed = examScore >= 60;
-        String name = "John willo feather";
+        // System.out.println("Hello World!");
+        // int examScore = 90;
+        // float examScore2 = 90.0f;
+        // boolean isPassed = examScore >= 60;
+        // String name = "John willo feather";
 
-        Test.displayResult(examScore, isPassed);
+        // Test.displayResult(examScore, isPassed);
+        Payment payment = new Payment("0241234567", "MTN", "GHS", "10");
+        System.out.println(payment.getPhoneNumber());
     }
 
     public static void another() {
---
+++ classwork/Payment.java
@@ -0,0 +1,47 @@
+public class Payment {
+    enum Network {
+        MTN, VODAFONE, TIGO, AIRTEL
+    }
+    
+    private String phoneNumber;
+    private Network network;
+    private String currency;
+    private String amount;
+
+    /**
+    * initial constructor for the payment class
+    * @param phoneNumber phone number for payment
+    * @param network network for payment
+    * @param currency currency for payment
+    * @param amount amount for payment
+    */
+
+    public Payment(String phoneNumber, Network network, String currency, String amount) {
+        this.network = network;
+        this.currency = currency;
+        this.amount = amount;
+        this.phoneNumber = formatPhoneNumber(phoneNumber);
+    }
+
+    /**
+    * format the phone number to the correct format
+    * @param phoneNumber phone number to be formatted
+    * @return formatted phone number
+    */
+
+    private String formatPhoneNumber(String phoneNumber) {
+        if (phoneNumber.startsWith("0") && phoneNumber.length() == 10) {
+            return "+233" + phoneNumber.substring(1, phoneNumber.length());
+        } else if (phoneNumber.startsWith("+233") && phoneNumber.length() == 13) {
+            return phoneNumber;
+        } 
+        return null;
+    }
+
+    /**
+    * @return the phoneNumber
+    */
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+}
